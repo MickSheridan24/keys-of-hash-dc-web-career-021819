d53436544c07edea750765cd76c72f92430ca218
Mickey <micksheridan.24@gmail.com>
2019-01-30 10:40:40 -0500

    submit

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    ret = []
+
+    arguments.each do |a|
+      self.each do |k, v|
+        if a == v
+          ret << k
+        end
+      end
+
+    end
+
+    ret
   end
 end
